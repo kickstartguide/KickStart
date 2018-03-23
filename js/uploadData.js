49924f6bc1c7270dab425f01a36bd43ffbcc59de
prahar4954 <prahar4954@github.com>
2018-03-23 12:22:28 +0530

    MOdified

--- js/uploadData.js
+++ js/uploadData.js
@@ -1,5 +1,6 @@
 var getData = function()
 {
+
 	var cityTag = document.querySelectorAll(".city");
 	var disciplineTag = document.querySelectorAll(".disciplines");
 	var branchTag = document.querySelectorAll(".branches");
@@ -22,23 +23,61 @@ var getData = function()
 
 	for (var i = branchTag.length - 1; i >= 0; i--) {
 		if(branchTag[i].checked)
-			branches.push(branchTag[i].value)
+			branches.push(branchTag[i].value);
 	}
 
 	for (var i = skillTag.length - 1; i >= 0; i--) 
 	{
 		if(skillTag[i].selected)
-			skillLevel=skillTag[i].value
+			skillLevel=skillTag[i].value;
 	}
 
-	var usersRef = firebase.database().ref("users/");
-	usersRef.push
-	({
-		"city":city,
+firebase.auth().onAuthStateChanged(function(user) {
+	var user = firebase.auth().currentUser;
+  if (user) {
+    
+    var userId = user.uid;
+    var name = user.displayName;
+    var email = user.email;
+
+var usersRef = firebase.database().ref("users/");
+var prefvalues = firebase.database().ref("users/"+userId+"/");
+prefvalues.set ({
+     "city":city,
 		"disciplines":disciplines,
 		"branches":branches,
 		"skill level":skillLevel
-	});
+});
 
 }
+});
+}
+var getDiscipline = function()
+{
+
+	$('.section1').addClass('hidden');
+	$('.section2').removeClass('hidden');
+	var disciplineTag = document.querySelectorAll(".disciplines");
+	
+	var disciplines = new Array();
+
+	
+
+	for (var i = disciplineTag.length - 1; i >= 0; i--) {
+		if(disciplineTag[i].checked)
+			disciplines.push(disciplineTag[i].value);
+	}
+
+	for(var i=disciplines.length - 1 ; i>=0 ; i--)
+	{
+		if(disciplines[i]=="Engineering")
+		{
+			$('.med').addClass('hidden');
+		}
+		else if(disciplines[i]=="Medical")
+		{
+			$('.eng').addClass('hidden');
+		}
+	}
+}	
 
